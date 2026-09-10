1f4f93287d36e309ad973743f0f8a06590071204
emma <sonicboss070@gmail.com>
2026-09-10 15:19:34 +0100

    rework on the crud api work

--- main.py
+++ main.py
@@ -141,19 +141,3 @@ def find_task_index(task_id: int):
         detail="Task not found"
     )
 
-def custom_exception_handler(request: Request, exc: Exception):
-    if isinstance(exc, HTTPException):
-        return JSONResponse(
-            status_code=exc.status_code,
-            content={"detail": exc.detail},
-        )
-    elif isinstance(exc, ValidationError):
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content={"detail": exc.errors()},
-        )
-    else:
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content={"detail": "Internal server error"},
-        )
